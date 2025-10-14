2a09cc4bb10906c0058428fcb6c81e5d0c9d6301
abhinandan998 <basuabhinandan99@gmail.com>
2025-10-14 19:26:38 +0530

    Coding Challenge Test1

---
+++ src/CodingChallengeTest1/Variable_Declaration_and_Initialization.java
@@ -0,0 +1,26 @@
+package CodingChallengeTest1;
+
+import java.util.Scanner;
+
+//Write a Java program that demonstrates proper variable declaration and initialization for different data types.
+//
+//**Requirements:**
+//- Declare and initialize variables of different primitive data types
+// (int, double, boolean, char)
+//- Print the values of all variables
+//- Show proper naming conventions
+public class Variable_Declaration_and_Initialization {
+    public static void main(String[] args) {
+
+        int age = 25;
+        double pi= 3.14;
+        boolean flag = true;
+        char ch ='A';
+
+        System.out.println("Integer: " +age);
+        System.out.println("Double: "+pi);
+        System.out.println("Boolean: "+flag);
+        System.out.println("Character:" +ch);
+
+    }
+}
