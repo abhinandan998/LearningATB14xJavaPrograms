dd16feef342848b374fc6b0b5082ff10d5b68c54
abhinandan998 <basuabhinandan99@gmail.com>
2025-11-07 16:20:42 +0530

    2nd NOV

---
+++ src/ex_28__Object/LabObject.java
@@ -0,0 +1,8 @@
+package ex_28__Object;
+
+public class LabObject {
+    static void main(String[] args) {
+
+        Object o = new Object();
+    }
+}
---
+++ src/ex_28__Object/LabSamples.java
@@ -0,0 +1,33 @@
+package ex_28__Object;
+
+public class LabSamples extends Object{
+
+    public LabSamples()
+    {
+        super();
+    }
+//    protected void finalize() throws Throwable {
+//        super.finalize();
+//    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
